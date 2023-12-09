99165fcf67ae6c8e1db578519d33cc6eef40798f
Lucio Anderlini <lucio.anderlini@fi.infn.it>
2023-12-09 12:28:35 +0100

    Trying shipping templates with code. v3

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setuptools.setup(
         ]
     },
     package_data={
-        '': ['icons/*', 'vkd_client/template/*.yaml'],
-        'vkd_client/template': ['vkd_client/template/*.yaml'],
+        '': ['icons/*', 'vkd_client/templates/*.yaml'],
+        'vkd_client/template': ['vkd_client/templatesu/*.yaml'],
     },
 )
