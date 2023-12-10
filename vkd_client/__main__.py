37ceb49fa1900e3f066c4186fe2ade26782a0816
Lucio Anderlini <lucio.anderlini@fi.infn.it>
2023-12-10 17:10:39 +0100

    Drafted log retrival

--- vkd_client/__main__.py
+++ vkd_client/__main__.py
@@ -4,7 +4,7 @@ from pathlib import Path
 import logging
 import textwrap
 from pprint import pprint
-from typing import List
+from typing import List, Union
 from datetime import datetime
 import pprint
 import yaml
@@ -19,6 +19,14 @@ import jinja2
 
 from cyclopts import App
 app = App()
+
+
+def _process_form_template(template: str, **kwargs):
+    processor = YamlProcessor()
+    with open(os.path.join(os.path.dirname(__file__), 'templates', f'{template}.yaml')) as template_file:
+        template = jinja2.Environment().from_string(template_file.read())
+    return processor.process(template.render(**kwargs))
+
 
 @app.command
 def request(input_file: Path):
@@ -46,14 +54,26 @@ def jobs(user: str = os.environ.get('JUPYTERHUB_USER'), queue: str = None):
     queue
         restrict list to a single queue
     """
-    processor = YamlProcessor()
-    with open(os.path.join(os.path.dirname(__file__), 'templates', 'jobs.yaml')) as template_file:
-        template = jinja2.Environment().from_string(template_file.read())
-    result = processor.process(template.render(
-        user=user,
-        queue=queue,
-    ))
-    pprint(result)
+    pprint(_process_form_template('jobs', user=user, queue=queue))
+
+@app.command
+def logs(job: str, index: Union[int, None] = None, container: Union[str, None] = None):
+    """
+    Retrieve and display the output of a job
+
+    Parameters
+    ----------
+    job
+        full name of the job
+    index
+        Optional, job completion index to display the output of a single iteration
+    container
+        Optional, the name of the container to display if not the main one
+    """
+    logs = _process_form_template('logs', job=job, index=index, container=container)
+    for pod_name, log in logs.items():
+        print(f"\n### Job Pod: {pod_name}")
+        print(log)
 
 @app.default
 def vkd():
